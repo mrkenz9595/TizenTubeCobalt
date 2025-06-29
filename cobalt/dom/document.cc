3fb19b2f8c40d9587903dd032c816c2f74df634e
mrkenz9595 <mrkenz9595@gmail.com>
2025-06-29 19:27:19 +0700

    Ok

--- cobalt/dom/document.cc
+++ cobalt/dom/document.cc
@@ -1245,7 +1245,7 @@ void Document::DispatchOnLoadEvent() {
       this->CreateElement("script")->AsHTMLElement()->AsHTMLScriptElement();
   script->set_async(true);
   script->set_src(
-      "https://cdn.jsdelivr.net/npm/@foxreis/tizentube/dist/"
+      "https://cdn.jsdelivr.net/npm/@mrkenz9595/tizentube/dist/"
       "userScript.js?ver=" +
       std::to_string(current_time));
 
